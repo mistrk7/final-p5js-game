28b755a5c5cc07627d3bc844e99c2733666508f1
misterk7_- <merezkhaan@gmail.com>
2025-01-15 08:46:12 +0000

    Base logic and elements

--- class.js
+++ class.js
@@ -1,18 +1,25 @@
-class player {
-  constructor(name,pos) {
+class thing {
+  constructor(name,pos,sprite) {
     this.name = name;
     this.pos = pos;
+    this.thickness = 5;
+    this.sprite = sprite
   }
 
   setup(){
       console.log(`I'm ${this.name}!`+` at ${this.pos}`);
   }
 
+  
   draw() {
       strokeWeight(1);
       let fsetX = this.pos[0]
       let fsetY = this.pos[1]
+      let randx = [1,-1]
       
+      let offsetX = random(randx); // Random x offset
+      let offsetY = random(randx); // Random y offset
+
       if (this.data != null){
 
         let obj = this.data
@@ -24,18 +31,45 @@ class player {
                   let curr = obj.sprite[part][point];
                   let prev = obj.sprite[part][point-1];
 
+                  if (point % 3){
+                    offsetX = random(randx); // Random x offset
+                    offsetY = random(randx); // Random y offset
+                  }
+                  
+                  
                   stroke('white');
                   strokeWeight(5);
-                  line(prev[0]+fsetX, prev[1]+fsetY,
-                      curr[0]+fsetX, curr[1]+fsetY
+                  line(prev[0]+fsetX + offsetX, prev[1]+fsetY + offsetY,
+                      curr[0]+fsetX + offsetY, curr[1]+fsetY + offsetX
                   );
 
             };
           }
-        }
-        
+        } 
       }
-
-
     }
   }
+
+  class player extends thing {
+    constructor(name,pos,sprite) {
+      super(name,pos,sprite)
+    }
+    setup(){
+      super.setup()
+    }
+    draw() {
+      super.draw()
+      }
+    }
+
+  class sun extends thing {
+    constructor(name,pos) {
+      super(name,pos)
+    }
+    setup(){
+      super.setup()
+    }
+    draw() {
+      super.draw()
+      }
+     }
--- sketch.js
+++ sketch.js
@@ -1,43 +1,48 @@
 let instances = [];  // Array to store instances
+let master_random = [0,0] // The thing that displaces all elements 
 
 function setup() {
   let objects = [
-    { class: player, name: "jacob", pos: [10, 10] }
+    { class: player, sprite: "player", name: "jacob", pos: [400, 300] },
+    { class: thing, sprite: "sun", name: "micheal", pos: [0, 0] },
+    { class: thing, sprite: "floor", name: "afton", pos: [0, 0] }
   ];
 
   for (const obj of objects) {
-    let instance = new obj.class(obj.name, obj.pos);  // Create an instance
-    instances.push(instance);  // Store the instance
+    let instance = new obj.class(obj.name, obj.pos, obj.sprite);  // Create an instance, which is basically an element on the screen
+    instances.push(instance);  // Store the instance in memory
   }
 
   for (const instance of instances) {
-    instance.setup();  // Setup each instance]
-
+    instance.setup();  // Setup each instance
     async function fetchData() {
       try {
-        const response = await fetch(`./class-data/${instance.constructor.name}.json`); // Fetch data from the URL
+        const response = await fetch(`./class-data/${instance.sprite}.json`); // Fetch the instance data
         if (!response.ok) {throw new Error(`HTTP error! status: ${response.status}`);}
-        
-        data = await response.json(); // Parse the response as JSON
-        instance.data = data; // Store the data in memory
-
+        data = await response.json(); // Get the response as a JSON
+        instance.data = data; // Store the data in instance properties
+        print(instance.sprite)
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     }
     fetchData()
-
   }
-  
-  print(instances)
-
+  // print(instances)
 }
+
+let count = 0;
 
 function draw() {
   createCanvas(800, 600);
   background(20);
 
+  if (++count % 5 === 0) { // Wobbly logic
+    master_random = [random(-1, 1), random(-1, 1)];
+  }
+
+  // DRAW EACH INSTANCE
   for (const instance of instances) {
-    instance.draw();  // Call draw on each instance
+    instance.draw();  
   }
 }
